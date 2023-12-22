a9612edf2a4b7c4ec92f6600ea4b0c6a66864a64
TamayoChristian <105449824+TamayoChristian@users.noreply.github.com>
2023-12-22 02:17:32 -0500

    Delete Ejercicio10_Interfaces directory

--- Ejercicio10_Interfaces/Mamifero.java
+++
@@ -1,8 +0,0 @@
-package Ejercicio10_Interfaces;
-/*
- *  Los mamíferos son una clase abstracta con el método abstracto 
- *  amamantar crías, que no devuelve nada
- */
-public abstract class Mamifero {
-	public abstract void Amamantar();
-}
--- Ejercicio10_Interfaces/Oviparos.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio10_Interfaces;
-/*
- *  Los animales ovíparos son una interfaz con el método poner huevos.
- */
-public interface Oviparos {
-	
-	public void PonerHuevos();
-
-}
