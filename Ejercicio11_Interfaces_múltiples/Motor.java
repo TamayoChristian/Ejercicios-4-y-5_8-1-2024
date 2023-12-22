11a87ab8aa16b9a39df87ad97bed8af6c98507f8
TamayoChristian <105449824+TamayoChristian@users.noreply.github.com>
2023-12-22 02:17:08 -0500

    Delete Ejercicio11_Interfaces_múltiples directory

--- Ejercicio11_Interfaces_múltiples/Motor.java
+++
@@ -1,5 +0,0 @@
-package Ejercicio11_Interfaces_múltiples;
-
-public interface Motor {
-	public int Calcular_Revoluciones(int Fuerza, int Radio);
-}
--- Ejercicio11_Interfaces_múltiples/Vela.java
+++
@@ -1,5 +0,0 @@
-package Ejercicio11_Interfaces_múltiples;
-
-public interface Vela {
-	public void Recomendar_Velocidad(int Velocidad_Viento);
-}
