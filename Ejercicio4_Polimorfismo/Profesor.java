ff2fa4cd701c1f541b3075b2715b86f9132e56c9
TamayoChristian <105449824+TamayoChristian@users.noreply.github.com>
2023-12-22 02:19:16 -0500

    Delete Ejercicio4_Polimorfismo directory

--- Ejercicio4_Polimorfismo/Profesor.java
+++
@@ -1,8 +0,0 @@
-package Ejercicio4_Polimorfismo;
-
-public class Profesor {
-	//Este metodo imprime el texo "Es un maestro" indicando que este es un objeto de tipo profesor
-	protected void Imprimir() {
-		System.out.println("Es un Profesor");
-	}
-}
--- Ejercicio4_Polimorfismo/ProfesorTitular.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio4_Polimorfismo;
-
-//subclase de la clase profesor
-public class ProfesorTitular extends Profesor{
-	//Metodo que indica que este es un profesor titular
-	protected void Imprimir() {
-		System.out.println("Es un profesor titular");
-	}
-}
