43591e6888d368dceabbf36c4063e39f60c4a819
TamayoChristian <105449824+TamayoChristian@users.noreply.github.com>
2023-12-22 02:20:04 -0500

    Delete Ejercicio12_Herencia_de_interfaces directory

--- Ejercicio12_Herencia_de_interfaces/MatchDeportivo.java
+++
@@ -1,6 +0,0 @@
-package Ejercicio12_Herencia_de_interfaces;
-
-public interface MatchDeportivo {
-	public void setEquipoLocal(String Nombre);
-	public void setEquipoVisitante(String Nombre);
-}
--- Ejercicio12_Herencia_de_interfaces/Partido_De_Baloncesto.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio12_Herencia_de_interfaces;
-
-public interface Partido_De_Baloncesto extends MatchDeportivo{
-	//Atributos
-	public double Duración = 40; //Duración del partido
-	//Métodos
-	public void setCestasEquipoLocal(int Marcador);
-	public void setCestasEquipoVisitante(int Marcador);
-}
